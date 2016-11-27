9caecd60d2f59913e226fc9d76e61f62b2a4aaef
Izaak Sofer <izaakrogan@gmail.com>
2016-11-27 17:28:15 +0000

    fit with basis

--- nm.py
+++ nm.py
@@ -69,6 +69,49 @@ ys = np.array ([-5.21659,2.53152,2.05687,14.1135,20.9673,33.5652])
 
 ft = findfit(2,xs,ys)
 
+# fit with basis
+
+# first define a list of functions
+basis = [lambda x: cos(2*pi*0*x),
+        lambda x: sin(2*pi*1*x),
+        lambda x: cos(2*pi*1*x),
+        lambda x: sin(2*pi*2*x),
+        lambda x: cos(2*pi*2*x)]
+
+# each element of this list is a function
+basis[3](1/10)
+
+# then modify a bit the code from lectures
+def findfitWithBasis(m,xs,ys):
+    A = zeros([m+1,m+1])
+    b = zeros(m+1)
+    for k in range(m+1):
+        b[k] = sum(ys*basis[k](xs)) # change here
+        for j in range(m+1):
+            A[k,j] = sum(basis[k](xs)*basis[j](xs)) # and here
+    a = linalg.solve(A,b)
+    def fit(x):
+        res = 0
+        for i in range(m+1):
+            res = res + a[i]*basis[i](x)
+        return res
+    return fit
+
+datax = arange(1,13)/12
+datay = array([9.2,8.7,8.2,9.6,11.4,13.6,15.4,16.9,17.3,16.3,14.7,12])
+ft=findfitWithBasis(3,datax,datay)
+
+xs = linspace(0,1)
+ys = list(map(ft,xs))
+plot(xs,ys,'b')
+plot(datax,datay,'or')
+
+# finding the maximum approximatelly from the picture
+[ft(0.72),ft(0.74),ft(0.73)]
+
+(0.74*12-8)*30
+# which is ~26 august
+
 # trapezoidal
 def trapezoidal(f,a,b,n):
     fs = list(map(lambda x: f(x), np.linspace(a,b,n+1)));
